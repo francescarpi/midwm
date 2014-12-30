24f7a09e09fc9695fd11c51f0236ad501b472d0d
Francesc Arpi <francesc.arpi@gmail.com>
2014-12-30 15:45:19 +0100

    Ordenando config

--- config.h
+++ config.h
@@ -1,43 +1,33 @@
-/* See LICENSE file for copyright and license details. */
 
-/* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-14-*-*-*-*-*-*-*";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#2c001e";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#dd4814";
-static const char selbgcolor[]      = "#dd4814";
-static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const Bool systraypinningfailfirst = True;   /* True: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const Bool showsystray       = True;     /* False means no systray */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const char font[]                        = "-*-terminus-medium-r-*-*-14-*-*-*-*-*-*-*";
+static const char normbordercolor[]             = "#444444";
+static const char normbgcolor[]                 = "#2c001e";
+static const char normfgcolor[]                 = "#bbbbbb";
+static const char selbordercolor[]              = "#dd4814";
+static const char selbgcolor[]                  = "#dd4814";
+static const char selfgcolor[]                  = "#eeeeee";
+static const unsigned int borderpx              = 1;            /* border pixel of windows */
+static const unsigned int snap                  = 32;           /* snap pixel */
+static const unsigned int systraypinning        = 0;            /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing        = 2;            /* systray spacing */
+static const Bool systraypinningfailfirst       = True;         /* True: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const Bool showsystray                   = True;         /* False means no systray */
+static const Bool showbar                       = True;         /* False means no bar */
+static const Bool topbar                        = True;         /* False means bottom bar */
 
-/* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class        instance    title       tags mask     isfloating   monitor */
-        {"fbpanel",     NULL,       NULL,       ~0,           False,       -1},
         {"galculator",  NULL,       NULL,       1 << 8,       True,        -1},
-        {"conky",       NULL,       NULL,       ~0,           True,        -1},
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+static const float mfact      = 0.55;   /* factor of master area size [0.05..0.95] */
+static const int nmaster      = 1;      /* number of clients in master area */
+static const Bool resizehints = False;  /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-	/* symbol     arrange function */
 	{ "[T]",      tile },    /* first entry is default */
 	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
